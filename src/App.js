ae0745477383221f79f41fec02cbf55584ffaa4e
Mouther <za7788az@hotmail.com>
2019-03-22 14:46:30 +0800

    logo + menu

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import { ReactComponent as Logo } from './img/logo.svg';
 import './App.css';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
+import Fab from '@material-ui/core/Fab';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AppBar>
-          <Toolbar>
-          </Toolbar>
-        </AppBar>
-        <MenuIcon />
+        <Fab id="fab">
+          <MenuIcon />
+        </Fab>
+        <Logo id="logo" />
+
       </div>
     );
   }
