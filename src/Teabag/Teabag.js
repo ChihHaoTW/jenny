e8565931ec080059f06b4af6c2a8a4847f8ae286
Mouther <za7788az@hotmail.com>
2019-04-01 19:46:50 +0800

    teabag intro

--- src/Teabag/Teabag.js
+++ src/Teabag/Teabag.js
@@ -7,7 +7,27 @@ class Teabag extends Component {
   render() {
 		return <div id="Teabag">
       <div className="intro">
+        <Typography className="has-sub" component="h4" variant="h4">
+          茶包 
+        </Typography>
+        <Typography component="h5" variant="h5">
+          煎藥符
+        </Typography>
+        <Typography variant="subtitle1" component="p">
+          煎藥符用法為搭配藥方煎煮後飲用
+        </Typography>
+        <Typography variant="subtitle1" component="p">
+          辟魍的茶包內含對抗不同亂象族群所需的功效
+        </Typography>
+        <Typography variant="subtitle1" component="p">
+          並將茶包包裝結合符咒元素設計
+        </Typography>
+        <Typography variant="subtitle1" component="p">
+          藉此讓消費者達到心靈慰藉以及身體上實質的益處
+        </Typography>
       </div>
+
+
 
 		</div>
   }
