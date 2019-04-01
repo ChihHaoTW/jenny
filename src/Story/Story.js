7382d5162b217c7eb2c477e3149e07fc4964237d
Mouther <za7788az@hotmail.com>
2019-04-01 10:11:16 +0800

    add Story

---
+++ src/Story/Story.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import './Story.css';
+
+class Story extends Component {
+  render() {
+		return <div id="Story">
+
+		</div>
+  }
+}
+
+export default Story;
